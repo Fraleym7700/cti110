5a847b4c42f4c684d0c4368bd0ba743ca24de74a
Fraleym7700 <54638784+Fraleym7700@users.noreply.github.com>
2019-09-03 13:08:26 -0400

    Add files via upload

---
+++ P3LAB_Debugging_MichaelFraley.py
@@ -0,0 +1,20 @@
+# CTI-110
+# P3LAB - Debugging
+# Fraley, Michael
+# 9/3/2019
+#
+score = int(input('what is your score:  '))
+A_score = (90)
+B_score = (80)
+C_score = (70)
+D_score = (60)
+if score >= A_score:
+    print('Your grade is A.')
+elif score >= B_score:
+    print('Your grade is B')
+elif score >= C_score:
+    print('Your grade is C')
+elif score >= D_score:
+    print('Your grade is D')
+else:
+    print('Your grade is F')
